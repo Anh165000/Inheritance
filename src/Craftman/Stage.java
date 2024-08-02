af1721e825eb07edf2ffba2367aad68d767849d6
Anh165000 <phamduyanh.01062005da@gmail.com>
2024-08-02 20:41:27 +0700

    add object square{data}, rectangle {data}, stage{craftman}

---
+++ src/Craftman/Stage.java
@@ -0,0 +1,10 @@
+package Craftman;
+
+import data.Rectangle;
+
+public class Stage {
+    public static void main(String[] args){
+        Rectangle a = new Rectangle("Anhh", "Blue", 10.4, 20.3);
+        a.paint();
+    }
+}
---
+++ src/data/Square.java
@@ -0,0 +1,45 @@
+package data;
+
+public class Square {
+
+    public String owner;
+    public String color;
+    public float edge;
+    public float area;
+    
+    public Square(String owner, String color, float edge, float area) {
+        this.owner = owner;
+        this.color = color;
+        this.edge = edge;
+        this.area = area;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public float getEdge() {
+        return edge;
+    }
+    public void setEdge(float edge) {
+        this.edge = edge;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    // public void setArea(float area) {
+    //     this.area = area;
+    // }
+
+    
+}
